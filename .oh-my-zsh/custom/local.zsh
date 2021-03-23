0a35885fc5846ff00b7f9f7a0b64f4d6612e9b90
Brian Dunn <brian.dunn@vineti.com>
2021-03-23 17:39:54 -0500

    from vineti mbp

--- .oh-my-zsh/custom/local.zsh
+++ .oh-my-zsh/custom/local.zsh
@@ -1,11 +1,17 @@
 export PATH=~/.bin:~/Library/Python/3.8/bin:$PATH
 export RUBY_EXTRA_CONFIGURE_OPTIONS="--enable-shared --install-doc"
-[ -z "${TMUX}" ] || export NVIM_LISTEN_ADDRESS="${TMPDIR}nvim-"`tmux display -p '#{session_id}#{window_id}' | tr $ s`
+if [ -z "${TMUX}" ]; then
+  export NVIM_LISTEN_ADDRESS=/tmp/nvimsocket
+else
+  export NVIM_LISTEN_ADDRESS="${TMPDIR}nvim-"`tmux display -p '#{session_id}#{window_id}' | tr $ s`
+fi
 # source $(brew --prefix asdf)/asdf.sh
 # hard coding because ^ is too slow
 source /usr/local/opt/asdf/asdf.sh
+export EDITOR='nvr -s'
 alias vim="nvim --listen ${NVIM_LISTEN_ADDRESS:-/tmp/nvimsocket}"
 alias be='bundle exec'
+alias cat=bat
 
 # git
 alias gap='git add --patch'
@@ -13,3 +19,4 @@ alias gd='git diff'
 alias gdc='git diff --cached'
 alias gpr='git pull --rebase'
 alias gra='git rebase --abort'
+alias grc='git rebase --continue'
